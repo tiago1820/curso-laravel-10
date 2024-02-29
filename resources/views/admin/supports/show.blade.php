c170c47e4cf94f799c35548a5fa26d8d3c9577d6
tiago1820 <tiago.zdo@gmail.com>
2024-02-29 01:31:53 -0300

    Aula 13 - Ok

---
+++ resources/views/admin/supports/show.blade.php
@@ -0,0 +1,7 @@
+<h1>Detalhes da dúvida {{ $support->id }}</h1>
+
+<ul>
+    <li>Assunto: {{ $support->subject }}</li>
+    <li>Status: {{ $support->status }}</li>
+    <li>Descriçao: {{ $support->body }}</li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
